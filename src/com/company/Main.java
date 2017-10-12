00a104ed84a8e05befe8b8a63f997f09db21a9cb
zoubeidaNM <zoumenz@yahoo.com>
2017-10-12 09:39:05 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        double x, y;
+
+        x = 3.1415;
+        y = 3.64;
+
+        System.out.println("pi is approximately " + x);
+        System.out.println("My GPA was " + y);
+    }
+}
